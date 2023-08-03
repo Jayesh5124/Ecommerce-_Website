c65d65595e0130253c5a6c881875e7f7e4cd009e
Jayesh Mahajan <124967010+Jayesh5124@users.noreply.github.com>
2023-08-03 15:37:32 +0530

    Delete Ecommerce/admin directory

--- Ecommerce/admin/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-$Id=$_GET['ID'];
-
-$con=mysqli_connect("localhost:3307","root","","ecommerce");
-mysqli_query($con,"DELETE FROM `tbluser` WHERE Id='$Id' ");
-header("location:user.php");
-?>
--- Ecommerce/admin/product/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo $Id=$_GET['ID'];
-include 'Config.php';
-mysqli_query($con,"DELETE FROM `tblproduct` WHERE id='$Id' ");
-header("location:index.php");
-?>
